d2bbb0c1de9bd2c55ff2407ab2580807c0e4d151
sofia tofigh <sst2140@columbia.edu>
2020-04-04 01:39:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  p
-  planeteer_calls.include?(x)
+  planeteer_calls.find do |x|
+  valid_calls.include?(x)
  end
 end
